0065c18c9df9afd42a24495c7fc85d2b8182f5ad
Damian <josepwnz@gmail.com>
2025-04-10 17:46:50 +0200

    test 100% pass

--- src/test/java/com/damian/xBank/xBankApplicationTests.java
+++ src/test/java/com/damian/xBank/xBankApplicationTests.java
@@ -2,13 +2,14 @@ package com.damian.xBank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "JWT_SECRET_KEY=THIS-IS-A-BIG-SECRET!-KEEP-IT-SAFE")
 class xBankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

f7f5965d8280dce19cd152e40131e5b219677dfb
Damian <josepwnz@gmail.com>
2025-04-12 01:55:12 +0200

    Permite mostrar los errores de validacion en el ApiResponse

--- src/main/java/com/damian/xBank/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/damian/xBank/common/exception/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.damian.xBank.common.exception;
 
 import com.damian.xBank.auth.exception.AuthenticationException;
+import com.damian.xBank.auth.exception.AuthorizationException;
 import com.damian.xBank.common.http.response.ApiResponse;
 import com.damian.xBank.customer.exception.CustomerException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,6 +25,25 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ApiResponse<String>> handleException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error("Validation error.", errors, HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<ApiResponse<String>> handleException(AuthorizationException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
     }

95a6ab6c8a0e5ae2640b002391b6975c691a4d7d
Damian <josepwnz@gmail.com>
2025-04-12 01:56:12 +0200

    Se elimina @Enumerated ya que solo funciona en entidades JPA

--- src/main/java/com/damian/xBank/profile/ProfileDTO.java
+++ src/main/java/com/damian/xBank/profile/ProfileDTO.java
@@ -1,15 +1,11 @@
 package com.damian.xBank.profile;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-
 public record ProfileDTO(
         Long id,
         String name,
         String surname,
         String phone,
         String birthdate,
-        @Enumerated(EnumType.STRING)
         Gender gender,
         String photo,
         String address,

27b42f636af361c2c756a1e50f7e7888dcc4e8c3
Damian <josepwnz@gmail.com>
2025-04-12 01:49:19 +0200

    Cambios para poder agregar los errores de validacion

--- src/main/java/com/damian/xBank/common/http/response/ApiResponse.java
+++ src/main/java/com/damian/xBank/common/http/response/ApiResponse.java
@@ -5,14 +5,23 @@ import org.springframework.http.HttpStatus;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
+    private String error;
     private String message;
     private T data;
     private HttpStatus status;
+
+    public ApiResponse() {
+
+    }
 
     public ApiResponse(String message, T data, HttpStatus status) {
         this.message = message;
         this.data = data;
         this.status = status;
+    }
+
+    public ApiResponse(String error) {
+        this.error = error;
     }
 
     public static <T> ApiResponse<T> success(T data) {
@@ -35,16 +44,43 @@ public class ApiResponse<T> {
         return new ApiResponse<>(message, null, status);
     }
 
+    public static <T> ApiResponse<T> error(String error) {
+        return new ApiResponse<>(error);
+    }
+
+    public static <T> ApiResponse<T> error(String error, T data, HttpStatus status) {
+        ApiResponse response = new ApiResponse();
+        response.setData(data);
+        response.setError(error);
+        response.setStatus(status);
+        return response;
+    }
+
     public T getData() {
         return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 
     public int getHttpCode() {
         return this.status.value();
     }
 
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
     public String getMessage() {
         return this.message;
     }
 
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }

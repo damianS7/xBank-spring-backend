276127df0ae32fd2e49335ca0dda5b0e3c375dbc
Damian <josepwnz@gmail.com>
2025-04-20 21:03:07 +0200

    se agrega validacion al request

--- src/main/java/com/damian/xBank/banking/account/http/request/BankingAccountOpenRequest.java
+++ src/main/java/com/damian/xBank/banking/account/http/request/BankingAccountOpenRequest.java
@@ -2,9 +2,13 @@ package com.damian.xBank.banking.account.http.request;
 
 import com.damian.xBank.banking.account.BankingAccountCurrency;
 import com.damian.xBank.banking.account.BankingAccountType;
+import jakarta.validation.constraints.NotNull;
 
 public record BankingAccountOpenRequest(
+        @NotNull(message = "Account type must not be null")
         BankingAccountType accountType,
+
+        @NotNull(message = "Account currency must not be null")
         BankingAccountCurrency accountCurrency
 ) {
 }

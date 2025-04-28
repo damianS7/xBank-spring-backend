662631484d234d88b617a126227e028e0412c907
Damian <josepwnz@gmail.com>
2025-04-28 17:07:39 +0200

    refactor

--- src/test/java/com/damian/xBank/banking/card/BankingCardIntegrationTest.java
+++ src/test/java/com/damian/xBank/banking/card/BankingCardIntegrationTest.java
@@ -159,7 +159,7 @@ public class BankingCardIntegrationTest {
         bankingAccount.setBalance(BigDecimal.valueOf(1000));
 
         BankingCard bankingCard = new BankingCard();
-        bankingCard.setLinkedBankingAccount(bankingAccount);
+        bankingCard.setBankingAccount(bankingAccount);
         bankingCard.setCardNumber("1234567890123");
 
         bankingAccount.addBankingCard(bankingCard);
@@ -196,7 +196,7 @@ public class BankingCardIntegrationTest {
         bankingCard.setCardType(BankingCardType.CREDIT);
         bankingCard.setCardNumber("1234567890123456");
         bankingCard.setCardStatus(BankingCardStatus.ENABLED);
-        bankingCard.setLinkedBankingAccount(bankingAccount);
+        bankingCard.setBankingAccount(bankingAccount);
 
         bankingAccount.addBankingCard(bankingCard);
         bankingAccountRepository.save(bankingAccount);

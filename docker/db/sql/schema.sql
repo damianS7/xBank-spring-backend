578fef6e832761c9ccebc5dee20dd343ecc3cee7
Damian <josepwnz@gmail.com>
2025-05-09 19:54:00 +0200

    testing > public

--- docker/db/sql/schema.sql
+++ docker/db/sql/schema.sql
@@ -1,30 +1,30 @@
-DROP SCHEMA IF EXISTS testing CASCADE;
+DROP SCHEMA IF EXISTS public CASCADE;
 
-CREATE SCHEMA testing AUTHORIZATION pg_database_owner;
+CREATE SCHEMA public AUTHORIZATION pg_database_owner;
 
-COMMENT ON SCHEMA testing IS 'standard testing schema';
+COMMENT ON SCHEMA public IS 'standard public schema';
 
--- DROP TYPE testing."auth_status_type";
+-- DROP TYPE public."auth_status_type";
 
-CREATE TYPE testing."auth_status_type" AS ENUM (
+CREATE TYPE public."auth_status_type" AS ENUM (
 	'DISABLED',
 	'ENABLED'
 );
 
 CREATE CAST (varchar as auth_status_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_account_currency_type";
+-- DROP TYPE public."banking_account_currency_type";
 
-CREATE TYPE testing."banking_account_currency_type" AS ENUM (
+CREATE TYPE public."banking_account_currency_type" AS ENUM (
 	'EUR',
 	'USD'
 );
 
 CREATE CAST (varchar as banking_account_currency_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_account_status_type";
+-- DROP TYPE public."banking_account_status_type";
 
-CREATE TYPE testing."banking_account_status_type" AS ENUM (
+CREATE TYPE public."banking_account_status_type" AS ENUM (
 	'OPEN',
 	'CLOSED',
 	'SUSPENDED'
@@ -32,9 +32,9 @@ CREATE TYPE testing."banking_account_status_type" AS ENUM (
 
 CREATE CAST (varchar as banking_account_status_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_account_transaction_status";
+-- DROP TYPE public."banking_account_transaction_status";
 
-CREATE TYPE testing."banking_transaction_status_type" AS ENUM (
+CREATE TYPE public."banking_transaction_status_type" AS ENUM (
 	'PENDING',
 	'FAILED',
 	'COMPLETED',
@@ -43,9 +43,9 @@ CREATE TYPE testing."banking_transaction_status_type" AS ENUM (
 
 CREATE CAST (varchar as banking_transaction_status_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_account_transaction_type";
+-- DROP TYPE public."banking_account_transaction_type";
 
-CREATE TYPE testing."banking_transaction_type" AS ENUM (
+CREATE TYPE public."banking_transaction_type" AS ENUM (
 	'DEPOSIT',
 	'WITHDRAWAL',
 	'CARD_CHARGE',
@@ -55,18 +55,18 @@ CREATE TYPE testing."banking_transaction_type" AS ENUM (
 
 CREATE CAST (varchar as banking_transaction_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_account_type";
+-- DROP TYPE public."banking_account_type";
 
-CREATE TYPE testing."banking_account_type" AS ENUM (
+CREATE TYPE public."banking_account_type" AS ENUM (
 	'SAVINGS',
 	'CHECK'
 );
 
 CREATE CAST (varchar as banking_account_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_card_status_type";
+-- DROP TYPE public."banking_card_status_type";
 
-CREATE TYPE testing."banking_card_status_type" AS ENUM (
+CREATE TYPE public."banking_card_status_type" AS ENUM (
 	'ENABLED',
 	'DISABLED',
 	'SUSPENDED'
@@ -74,36 +74,36 @@ CREATE TYPE testing."banking_card_status_type" AS ENUM (
 
 CREATE CAST (varchar as banking_card_status_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."banking_card_type";
+-- DROP TYPE public."banking_card_type";
 
-CREATE TYPE testing."banking_card_type" AS ENUM (
+CREATE TYPE public."banking_card_type" AS ENUM (
 	'CREDIT',
 	'DEBIT'
 );
 
 CREATE CAST (varchar as banking_card_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."customer_gender_type";
+-- DROP TYPE public."customer_gender_type";
 
-CREATE TYPE testing."customer_gender_type" AS ENUM (
+CREATE TYPE public."customer_gender_type" AS ENUM (
 	'MALE',
 	'FEMALE'
 );
 
 CREATE CAST (varchar as customer_gender_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."customer_role";
+-- DROP TYPE public."customer_role";
 
-CREATE TYPE testing."customer_role_type" AS ENUM (
+CREATE TYPE public."customer_role_type" AS ENUM (
 	'CUSTOMER',
 	'ADMIN'
 );
 
 CREATE CAST (varchar as customer_role_type) WITH INOUT AS IMPLICIT;
 
--- DROP TYPE testing."email_verification_status_type";
+-- DROP TYPE public."email_verification_status_type";
 
-CREATE TYPE testing."email_verification_status_type" AS ENUM (
+CREATE TYPE public."email_verification_status_type" AS ENUM (
 	'NOT_VERIFIED',
 	'VERIFIED'
 );
@@ -112,12 +112,12 @@ CREATE CAST (varchar as email_verification_status_type) WITH INOUT AS IMPLICIT;
 
 -- Drop table
 
--- DROP TABLE testing.customers;
+-- DROP TABLE public.customers;
 
-CREATE TABLE testing.customers (
+CREATE TABLE public.customers (
 	id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
 	email varchar(80) NOT NULL,
-	"role" testing."customer_role_type" DEFAULT 'CUSTOMER'::customer_role_type NOT NULL,
+	"role" public."customer_role_type" DEFAULT 'CUSTOMER'::customer_role_type NOT NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	CONSTRAINT customers_email_key UNIQUE (email),
@@ -125,62 +125,62 @@ CREATE TABLE testing.customers (
 );
 
 
--- testing.banking_accounts definition
+-- public.banking_accounts definition
 
 -- Drop table
 
--- DROP TABLE testing.banking_accounts;
+-- DROP TABLE public.banking_accounts;
 
-CREATE TABLE testing.banking_accounts (
+CREATE TABLE public.banking_accounts (
 	id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
 	customer_id int4 NOT NULL,
 	account_number varchar(32) NOT NULL,
 	balance numeric(15, 2) DEFAULT 0.00 NOT NULL,
-	account_type testing."banking_account_type" DEFAULT 'SAVINGS'::banking_account_type NOT NULL,
-	account_currency testing."banking_account_currency_type" DEFAULT 'EUR'::banking_account_currency_type NOT NULL,
-	account_status testing."banking_account_status_type" DEFAULT 'CLOSED'::banking_account_status_type NOT NULL,
+	account_type public."banking_account_type" DEFAULT 'SAVINGS'::banking_account_type NOT NULL,
+	account_currency public."banking_account_currency_type" DEFAULT 'EUR'::banking_account_currency_type NOT NULL,
+	account_status public."banking_account_status_type" DEFAULT 'CLOSED'::banking_account_status_type NOT NULL,
 	notes text NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	CONSTRAINT banking_accounts_account_number_key UNIQUE (account_number),
 	CONSTRAINT banking_accounts_pkey PRIMARY KEY (id),
-	CONSTRAINT banking_accounts_customers_fk FOREIGN KEY (customer_id) REFERENCES testing.customers(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT banking_accounts_customers_fk FOREIGN KEY (customer_id) REFERENCES public.customers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
--- testing.customer_auth definition
+-- public.customer_auth definition
 
 -- Drop table
 
--- DROP TABLE testing.customer_auth;
+-- DROP TABLE public.customer_auth;
 
-CREATE TABLE testing.customer_auth (
+CREATE TABLE public.customer_auth (
 	id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
 	customer_id int4 NOT NULL,
 	password_hash varchar(60) NOT NULL,
-	auth_account_status testing."auth_status_type" DEFAULT 'ENABLED'::auth_status_type NOT NULL,
-	"email_verification_status" testing."email_verification_status_type" DEFAULT 'NOT_VERIFIED'::email_verification_status_type NOT NULL,
+	auth_account_status public."auth_status_type" DEFAULT 'ENABLED'::auth_status_type NOT NULL,
+	"email_verification_status" public."email_verification_status_type" DEFAULT 'NOT_VERIFIED'::email_verification_status_type NOT NULL,
 	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	CONSTRAINT auth_customer_id_key UNIQUE (customer_id),
 	CONSTRAINT auth_pkey PRIMARY KEY (id),
-	CONSTRAINT auth_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES testing.customers(id) ON DELETE CASCADE
+	CONSTRAINT auth_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES public.customers(id) ON DELETE CASCADE
 );
 
 
--- testing.customer_profiles definition
+-- public.customer_profiles definition
 
 -- Drop table
 
--- DROP TABLE testing.customer_profiles;
+-- DROP TABLE public.customer_profiles;
 
-CREATE TABLE testing.customer_profiles (
+CREATE TABLE public.customer_profiles (
 	id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
 	customer_id int4 NOT NULL,
 	"name" varchar(20) NOT NULL,
 	surname varchar(40) NOT NULL,
 	phone varchar(14) NOT NULL,
 	birthdate date NOT NULL,
-	gender testing."customer_gender_type" NOT NULL,
+	gender public."customer_gender_type" NOT NULL,
 	photo_path varchar(100) NULL, -- path to image
 	address varchar(50) NOT NULL,
 	postal_code varchar(8) NOT NULL,
@@ -190,49 +190,49 @@ CREATE TABLE testing.customer_profiles (
 	CONSTRAINT profiles_customer_id_key UNIQUE (customer_id),
 	CONSTRAINT profiles_national_id_key UNIQUE (national_id),
 	CONSTRAINT profiles_pkey PRIMARY KEY (id),
-	CONSTRAINT profiles_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES testing.customers(id) ON DELETE CASCADE
+	CONSTRAINT profiles_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES public.customers(id) ON DELETE CASCADE
 );
 
 -- Column comments
 
-COMMENT ON COLUMN testing.customer_profiles.photo_path IS 'path to image';
+COMMENT ON COLUMN public.customer_profiles.photo_path IS 'path to image';
 
 
--- testing.banking_cards definition
+-- public.banking_cards definition
 
 -- Drop table
 
--- DROP TABLE testing.banking_cards;
+-- DROP TABLE public.banking_cards;
 
-CREATE TABLE testing.banking_cards (
+CREATE TABLE public.banking_cards (
 	id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
 	banking_account_id int4 NOT NULL,
-	card_type testing."banking_card_type" NOT NULL,
-	card_status testing."banking_card_status_type" DEFAULT 'DISABLED'::banking_card_status_type NOT NULL,
+	card_type public."banking_card_type" NOT NULL,
+	card_status public."banking_card_status_type" DEFAULT 'DISABLED'::banking_card_status_type NOT NULL,
 	card_number varchar(32) NOT NULL,
 	notes text NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	CONSTRAINT banking_card_pkey PRIMARY KEY (id),
-	CONSTRAINT banking_card_banking_account_id_fkey FOREIGN KEY (banking_account_id) REFERENCES testing.banking_accounts(id) ON DELETE CASCADE
+	CONSTRAINT banking_card_banking_account_id_fkey FOREIGN KEY (banking_account_id) REFERENCES public.banking_accounts(id) ON DELETE CASCADE
 );
 
 
--- testing.banking_transactions definition
+-- public.banking_transactions definition
 
 -- Drop table
 
--- DROP TABLE testing.banking_transactions;
+-- DROP TABLE public.banking_transactions;
 
-CREATE TABLE testing.banking_transactions (
+CREATE TABLE public.banking_transactions (
 	id int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
 	banking_account_id int4 NOT NULL,
-	transaction_type testing."banking_transaction_type" NOT NULL,
+	transaction_type public."banking_transaction_type" NOT NULL,
 	amount numeric(15, 2) NOT NULL,
 	description text NULL,
-	transaction_status testing."banking_transaction_status_type" DEFAULT 'PENDING'::banking_transaction_status_type NOT NULL,
+	transaction_status public."banking_transaction_status_type" DEFAULT 'PENDING'::banking_transaction_status_type NOT NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	updated_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	CONSTRAINT banking_transactions_pkey PRIMARY KEY (id),
-	CONSTRAINT banking_transactions_banking_account_id_fkey FOREIGN KEY (banking_account_id) REFERENCES testing.banking_accounts(id) ON DELETE CASCADE
+	CONSTRAINT banking_transactions_banking_account_id_fkey FOREIGN KEY (banking_account_id) REFERENCES public.banking_accounts(id) ON DELETE CASCADE
 );

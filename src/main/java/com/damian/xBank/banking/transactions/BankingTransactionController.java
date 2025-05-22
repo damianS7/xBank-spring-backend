c021e22cc18ef65abbdf94d3b8465286953c8770
Damian <josepwnz@gmail.com>
2025-05-23 00:25:35 +0200

    Controller split up. now admin and customers have different manage endpoint on different Controllers

--- src/main/java/com/damian/xBank/banking/transactions/BankingTransactionController.java
+++ src/main/java/com/damian/xBank/banking/transactions/BankingTransactionController.java
@@ -1,12 +1,8 @@
 package com.damian.xBank.banking.transactions;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1")
 @RestController
@@ -18,25 +14,5 @@ public class BankingTransactionController {
         this.bankingTransactionService = bankingTransactionService;
     }
 
-    // endpoint to patch a transaction field
-    @PatchMapping("/admin/banking/transactions/{id}")
-    public ResponseEntity<?> patchTransaction(
-            @PathVariable @NotNull(message = "This field cannot be null") @Positive
-            Long id,
-            @Validated @RequestBody
-            BankingTransactionPatchRequest request
-    ) {
-        BankingTransaction bankingTransaction = bankingTransactionService.patchStatusTransaction(
-                id,
-                request
-        );
-
-        BankingTransactionDTO bankingTransactionDTO = BankingTransactionDTOMapper
-                .toBankingTransactionDTO(bankingTransaction);
-
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(bankingTransactionDTO);
-    }
 }
 
---
+++ src/main/java/com/damian/xBank/banking/transactions/admin/BankingTransactionAdminController.java
@@ -0,0 +1,43 @@
+package com.damian.xBank.banking.transactions.admin;
+
+import com.damian.xBank.banking.transactions.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/v1")
+@RestController
+public class BankingTransactionAdminController {
+    private final BankingTransactionService bankingTransactionService;
+
+    @Autowired
+    public BankingTransactionAdminController(BankingTransactionService bankingTransactionService) {
+        this.bankingTransactionService = bankingTransactionService;
+    }
+
+    // endpoint to patch a transaction field
+    @PatchMapping("/admin/banking/transactions/{id}")
+    public ResponseEntity<?> patchTransaction(
+            @PathVariable @NotNull(message = "This field cannot be null") @Positive
+            Long id,
+            @Validated @RequestBody
+            BankingTransactionPatchRequest request
+    ) {
+        BankingTransaction bankingTransaction = bankingTransactionService.patchStatusTransaction(
+                id,
+                request
+        );
+
+        BankingTransactionDTO bankingTransactionDTO = BankingTransactionDTOMapper
+                .toBankingTransactionDTO(bankingTransaction);
+
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(bankingTransactionDTO);
+    }
+}
+

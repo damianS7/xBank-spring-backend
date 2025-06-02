e9769d12b72f74b68ba2c6f3ac46bf6e596f577b
Damian <josepwnz@gmail.com>
2025-06-03 00:15:38 +0200

    profile service cleanup code

--- src/main/java/com/damian/xBank/customer/profile/ProfileService.java
+++ src/main/java/com/damian/xBank/customer/profile/ProfileService.java
@@ -1,17 +1,15 @@
 package com.damian.xBank.customer.profile;
 
-import com.damian.xBank.common.exception.PasswordMismatchException;
+import com.damian.xBank.common.utils.AuthUtils;
+import com.damian.xBank.common.utils.ProfileUtils;
 import com.damian.xBank.customer.Customer;
 import com.damian.xBank.customer.CustomerGender;
-import com.damian.xBank.customer.CustomerRole;
 import com.damian.xBank.customer.profile.exception.ProfileAuthorizationException;
 import com.damian.xBank.customer.profile.exception.ProfileException;
 import com.damian.xBank.customer.profile.exception.ProfileNotFoundException;
-import com.damian.xBank.customer.profile.http.request.ProfilePatchRequest;
 import com.damian.xBank.customer.profile.http.request.ProfileUpdateRequest;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -27,148 +25,100 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ProfileService {
-
+    // TODO move to ProfileAvatarService
+    private final String PROFILE_IMAGE_PATH = "uploads/profile/images";
+    private final long MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
     private final ProfileRepository profileRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public ProfileService(ProfileRepository profileRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public ProfileService(
+            ProfileRepository profileRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder
+    ) {
         this.profileRepository = profileRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public Resource getPhoto(String filename) {
-        Path filePath = Paths.get("uploads/avatars").resolve(filename).normalize();
-        System.out.println(filePath);
-        Resource resource = null;
-        try {
-            resource = new UrlResource(filePath.toUri());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (!resource.exists()) {
-            //            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Archivo no encontrado");
-        }
-
-        return resource;
-
+    /**
+     * returns a profile
+     *
+     * @param profileId the profile id
+     * @return Profile the profile
+     */
+    public Profile getProfile(Long profileId) {
+        return profileRepository
+                .findById(profileId)
+                .orElseThrow(
+                        () -> new ProfileNotFoundException(ProfileNotFoundException.NOT_FOUND)
+                );
     }
 
-    /**
-     * It sets the customer profile photo
-     */
-    public Profile uploadPhoto(String currentPassword, MultipartFile file) {
-        //        if (file.isEmpty()) {
-        //            throw new IllegalArgumentException("El archivo está vacío");
-        //        }
-        //
-        //        if (!file.getContentType().startsWith("image/")) {
-        //            throw new IllegalArgumentException("Solo se permiten archivos de imagen");
-        //        }
-        //
-        //        if (file.getSize() > 5 * 1024 * 1024) { // 5 MB
-        //            throw new IllegalArgumentException("El archivo no debe superar los 5 MB");
-        //        }
-        Customer customerLogged = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        final Long profileId = customerLogged.getProfile().getId();
+    // validations for file uploaded photos
+    private void validatePhotoOrElseThrow(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new ProfileAuthorizationException(
+                    ProfileException.PROFILE_IMAGE.EMPTY_FILE
+            );
+        }
 
-        // ...
-        String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
-        String filename = UUID.randomUUID() + "." + extension;
+        if (!file.getContentType().startsWith("image/")) {
+            throw new ProfileAuthorizationException(
+                    ProfileException.PROFILE_IMAGE.ONLY_IMAGES_ALLOWED
+            );
+        }
 
-        Map<String, Object> fieldsToUpdate = new HashMap<>();
+        if (file.getSize() > MAX_FILE_SIZE) { // 5 MB
+            throw new ProfileAuthorizationException(
+                    ProfileException.PROFILE_IMAGE.FILE_SIZE_LIMIT
+            );
+        }
+    }
 
-        // Guardar el archivo localmente (puedes adaptarlo a guardar en S3, DB, etc.)
+    // stores the image
+    private void storeFile(MultipartFile file, String filename) {
         try {
-            Path uploadPath = Paths.get("uploads/avatars");
+            Path uploadPath = Paths.get(PROFILE_IMAGE_PATH);
             Files.createDirectories(uploadPath);
             Path filePath = uploadPath.resolve(filename);
             Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-            fieldsToUpdate.put("photoPath", filename);
         } catch (IOException e) {
-            throw new ProfileException("Error al subir archivo");
+            throw new ProfileAuthorizationException(
+                    ProfileException.PROFILE_IMAGE.UPLOAD_FAILED
+            );
         }
-
-
-        ProfilePatchRequest patchRequest = new ProfilePatchRequest(
-                currentPassword,
-                fieldsToUpdate
-        );
-
-        return patchProfile(profileId, patchRequest);
     }
 
-    /**
-     * It updates the whole customer profile
-     *
-     * @param profileId the id of the profile to update
-     * @param request   the fields to change
-     * @return the profile updated
-     */
+    // it updates the logged customer profile
+    public Profile updateProfile(ProfileUpdateRequest request) {
+        final Customer customerLogged = AuthUtils.getLoggedCustomer();
+
+        // validate password
+        AuthUtils.validatePasswordOrElseThrow(request.currentPassword(), customerLogged);
+
+        return this.updateProfile(customerLogged.getProfile().getId(), request);
+    }
+
+    // it updates a profile by id
     public Profile updateProfile(Long profileId, ProfileUpdateRequest request) {
-        Map<String, Object> fieldsToUpdate = new HashMap<>();
-        fieldsToUpdate.put("firstName", request.name());
-        fieldsToUpdate.put("lastName", request.surname());
-        fieldsToUpdate.put("phone", request.phone());
-        fieldsToUpdate.put("address", request.address());
-        fieldsToUpdate.put("country", request.country());
-        fieldsToUpdate.put("postalCode", request.postalCode());
-        fieldsToUpdate.put("photoPath", request.photoPath());
-        fieldsToUpdate.put("nationalId", request.nationalId());
-        fieldsToUpdate.put("gender", request.gender().toString());
-        fieldsToUpdate.put("birthdate", request.birthdate().toString());
-
-        ProfilePatchRequest patchRequest = new ProfilePatchRequest(
-                request.currentPassword(),
-                fieldsToUpdate
-        );
-
-        return patchProfile(profileId, patchRequest);
-    }
-
-    public Profile patchCustomerProfile(ProfilePatchRequest request) {
-        return this.patchProfile(null, request);
-    }
-
-    public Profile patchProfile(Long profileId, ProfilePatchRequest request) {
-        // We get the customer logged in the context
-        Customer customerLogged = (Customer) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        // if profiledId is null, then is a customer modifying its own profile.
-        if (profileId == null) {
-            profileId = Optional.ofNullable(customerLogged.getProfile())
-                                .map(Profile::getId)
-                                .orElseThrow(() -> new ProfileException("Cannot access to customer profile.")
-                                );
-        }
-
         // We get the profile we want to modify
         Profile profile = profileRepository
                 .findById(profileId)
                 .orElseThrow(
-                        ProfileNotFoundException::new
+                        () -> new ProfileNotFoundException(
+                                ProfileException.NOT_FOUND
+                        )
                 );
+
+        final Customer customerLogged = AuthUtils.getLoggedCustomer();
 
         // if the logged user is not admin
-        if (!customerLogged.getRole().equals(CustomerRole.ADMIN)) {
-            // before making any changes we check that the user sent the current password.
-            if (!bCryptPasswordEncoder.matches(request.currentPassword(), customerLogged.getPassword())) {
-                throw new PasswordMismatchException(
-                        PasswordMismatchException.PASSWORD_MISMATCH
-                );
-            }
-
+        if (!AuthUtils.isAdmin(customerLogged)) {
             // we make sure that this profile belongs to the customer logged
-            if (!profile.getCustomerId().equals(customerLogged.getId())) {
-                throw new ProfileAuthorizationException(
-                        ProfileAuthorizationException.PROFILE_NOT_BELONG_TO_CUSTOMER
-                );
-            }
+            ProfileUtils.hasAuthorizationOrElseThrow(profile, customerLogged);
         }
 
         // we iterate over the fields (if any)
@@ -184,7 +134,9 @@ public class ProfileService {
                 case "nationalId" -> profile.setNationalId((String) value);
                 case "gender" -> profile.setGender(CustomerGender.valueOf((String) value));
                 case "birthdate" -> profile.setBirthdate(LocalDate.parse((String) value));
-                default -> throw new ProfileException("Field '" + key + "' is not updatable.");
+                default -> throw new ProfileAuthorizationException(
+                        ProfileException.INVALID_FIELD
+                );
             }
         });
 
@@ -192,5 +144,54 @@ public class ProfileService {
         profile.setUpdatedAt(Instant.now());
 
         return profileRepository.save(profile);
+    }
+
+    // returns the profile photo as Resource
+    public Resource getPhoto(String filename) {
+        Path filePath = Paths.get(PROFILE_IMAGE_PATH).resolve(filename).normalize();
+        Resource resource = null;
+        try {
+            resource = new UrlResource(filePath.toUri());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (!resource.exists()) {
+            throw new ProfileAuthorizationException(
+                    ProfileException.PROFILE_IMAGE.NOT_FOUND
+            );
+        }
+        return resource;
+    }
+
+
+    /**
+     * It sets the customer profile photo
+     */
+    public Profile uploadPhoto(String currentPassword, MultipartFile file) {
+        final Customer customerLogged = AuthUtils.getLoggedCustomer();
+
+        // validate password
+        AuthUtils.validatePasswordOrElseThrow(currentPassword, AuthUtils.getLoggedCustomer());
+
+        // run file validations
+        this.validatePhotoOrElseThrow(file);
+
+        final String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        final String filename = UUID.randomUUID() + "." + extension;
+
+        // saving file
+        this.storeFile(file, filename);
+
+        Map<String, Object> fieldsToUpdate = new HashMap<>();
+        fieldsToUpdate.put("photoPath", filename);
+        ProfileUpdateRequest patchRequest = new ProfileUpdateRequest(
+                currentPassword,
+                fieldsToUpdate
+        );
+
+        final Long profileId = customerLogged.getProfile().getId();
+
+        return updateProfile(profileId, patchRequest);
     }
 }

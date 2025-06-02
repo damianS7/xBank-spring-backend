7f2bfb88c997fdae0e30d71c21287df173e01414
Damian <josepwnz@gmail.com>
2025-06-03 00:13:31 +0200

    deleted and renamed

--- src/main/java/com/damian/xBank/customer/profile/http/request/ProfileUpdateRequest.java
+++ src/main/java/com/damian/xBank/customer/profile/http/request/ProfileUpdateRequest.java
@@ -1,41 +1,13 @@
 package com.damian.xBank.customer.profile.http.request;
 
-import com.damian.xBank.customer.CustomerGender;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-import java.time.LocalDate;
+import java.util.Map;
 
 public record ProfileUpdateRequest(
-        @NotBlank(message = "Name must not be blank.")
-        String name,
-
-        @NotBlank(message = "Surname must not be blank.")
-        String surname,
-
-        @NotBlank(message = "Phone must not be blank.")
-        String phone,
-
-        @NotNull(message = "Birthdate must not be null.")
-        LocalDate birthdate,
-
-        @NotNull(message = "Gender must not be null")
-        CustomerGender gender,
-
-        String photoPath,
-
-        @NotBlank(message = "Address must not be blank.")
-        String address,
-
-        @NotBlank(message = "Postal code must not be blank.")
-        String postalCode,
-
-        @NotBlank(message = "Country must not be blank.")
-        String country,
-
-        @NotBlank(message = "National ID must not be blank.")
-        String nationalId,
-
         @NotBlank
-        String currentPassword) {
+        String currentPassword,
+
+        Map<String, Object> fieldsToUpdate
+) {
 }

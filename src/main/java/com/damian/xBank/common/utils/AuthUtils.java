5497bf5bdf81a81b8d8e759aca7123962a8a74b2
Damian <josepwnz@gmail.com>
2025-06-02 18:31:49 +0200

    renamed AuthCustomer.java to AuthUtils. Added method.

--- src/main/java/com/damian/xBank/common/utils/AuthUtils.java
+++ src/main/java/com/damian/xBank/common/utils/AuthUtils.java
@@ -1,11 +1,18 @@
 package com.damian.xBank.common.utils;
 
+import com.damian.xBank.common.exception.PasswordMismatchException;
 import com.damian.xBank.customer.Customer;
 import com.damian.xBank.customer.CustomerRole;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class AuthCustomer {
+public class AuthUtils {
+    public static void validatePasswordOrElseThrow(String rawPassword, Customer customer) {
+        if (!AuthUtils.isPasswordCorrect(rawPassword, customer.getAuth().getPassword())) {
+            throw new PasswordMismatchException(PasswordMismatchException.PASSWORD_MISMATCH);
+        }
+    }
+
     public static boolean isPasswordCorrect(String rawPassword, String hashedPassword) {
         final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder.matches(rawPassword, hashedPassword);

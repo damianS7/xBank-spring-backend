d69754e4b4c9e0d83742235026dd314428134269
Damian <josepwnz@gmail.com>
2025-06-04 20:11:22 +0200

    added default currency

--- src/main/java/com/damian/xBank/banking/account/BankingAccount.java
+++ src/main/java/com/damian/xBank/banking/account/BankingAccount.java
@@ -57,6 +57,7 @@ public class BankingAccount {
         this.accountTransactions = new HashSet<>();
         this.bankingCards = new HashSet<>();
         this.balance = BigDecimal.valueOf(0);
+        this.accountCurrency = BankingAccountCurrency.EUR;
         this.accountType = BankingAccountType.SAVINGS;
         this.accountStatus = BankingAccountStatus.OPEN;
         this.createdAt = Instant.now();
